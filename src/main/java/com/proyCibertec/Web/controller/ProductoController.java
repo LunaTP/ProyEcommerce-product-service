6ac2f01e147bc196c8761ad7f21df44f866a5aaa
Diego Alejandro Luna Tarapa <diegolt2014@gmail.com>
2025-07-06 08:53:46 -0500

    a

--- src/main/java/com/proyCibertec/Web/controller/ProductoController.java
+++ src/main/java/com/proyCibertec/Web/controller/ProductoController.java
@@ -14,27 +14,27 @@ public class ProductoController {
 
     private final ProductoService productoService;
 
-    @GetMapping
+    @GetMapping("/listar")
     public List<ProductoResponseDto> listarTodos() {
         return productoService.getAllProductos();
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/buscar/{id}")
     public ProductoResponseDto buscarPorId(@PathVariable Integer id) {
         return productoService.getProductoById(id);
     }
 
-    @PostMapping
+    @PostMapping("/crear")
     public void registrar(@RequestBody ProductoResponseDto dto) {
         productoService.createProducto(dto);
     }
 
-    @PutMapping
+    @PutMapping("/actualizar")
     public void actualizar(@RequestBody ProductoResponseDto dto) {
         productoService.updateProducto(dto);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/eliminar/{id}")
     public void eliminar(@PathVariable Integer id) {
         productoService.deleteProducto(id);
     }
